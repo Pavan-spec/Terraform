b47add2bd0af284a6a074b5e35e7ca2a520abb34
Pavan-spec <58391452+Pavan-spec@users.noreply.github.com>
2024-01-17 15:37:55 +0530

    Update Main.tf

--- Day-9/Project-Minikube/Main.tf
+++ Day-9/Project-Minikube/Main.tf
@@ -1,1 +1,74 @@
+# Define the AWS provider configuration.
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region.
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "terraform-demo-abhi"  # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+resource "aws_security_group" "webSg" {
+  name   = "web"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+
+resource "aws_instance" "server" {
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.medium"
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install docker.io -y",
+      "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+      "sudo chmod +x /home/ubuntu/kubectl",
+      "sudo cp kubectl /usr/local/bin/kubectl",
+      "sudo usermod -aG docker ubuntu"
+    ]
+  }
+}
+
+
 
